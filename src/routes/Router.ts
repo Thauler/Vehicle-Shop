f7211dfe7dbd02fba659230e8c757f0aa3fe67d0
Thauler <thauler98@gmail.com>
2022-07-06 22:44:27 -0300

    feat(routes/router): add a router file
    creates a generic router to provide routes for the server

---
+++ src/routes/Router.ts
@@ -0,0 +1,14 @@
+import { Router } from 'express';
+import Controller from '../controllers';
+
+export default class Routers<T> {
+  public router: Router;
+
+  constructor() {
+    this.router = Router();
+  }
+
+  public addRoute(controller: Controller<T>, route: string = controller.route) {
+    this.router.post(route, controller.create);
+  }
+}
