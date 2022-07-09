d322274b52ad46fc7878da1279dafafcbd325916
Thauler <thauler98@gmail.com>
2022-07-09 12:12:44 -0300

    refactor(carmodel.test.ts): abstract class
    after the abstraction of the model layer at the model test i needed to put the mongoose like a
    parameter when car model is intanced

--- src/tests/unit/models/CarModel.test.ts
+++ src/tests/unit/models/CarModel.test.ts
@@ -1,17 +1,22 @@
 import { expect } from 'chai';
-import mongoose from 'mongoose';
-import sinon from 'sinon';
+import sinon, { SinonStub } from 'sinon';
+import * as carSchema from '../../../models/Schemas/CarSchema';
 import CarModel from '../../../models/CarModel';
-import { carMockRequest, carMockResponse } from '../mocks/CarMock';
+import { carMockRequest, carMockResponse } from '../../mocks/CarMock';
+
+const carModel = new CarModel(carSchema.carMongooseModel);
 
 describe('1 - Testing car model', () => {
+
   describe('A - Testing mehtod creates', () => {
+
     before(async () => {
-      sinon.stub(mongoose.Model, 'create').resolves(carMockResponse);
+      sinon.stub(carSchema.carMongooseModel, 'create').resolves(carMockResponse);
     });
-    after(() => sinon.restore());
+
+    after(() => (carSchema.carMongooseModel.create as SinonStub).restore());
+
     it('In case of success', async () => {
-      const carModel = new CarModel();
       const createdCar = await carModel.create(carMockRequest);
 
       expect(createdCar).to.be.an('object');
