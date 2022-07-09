525be58a0f4d044ac62460a085f036891e1df751
Thauler <thauler98@gmail.com>
2022-07-09 12:02:24 -0300

    feat(carmock): mock error resquest
    add a car mock error request

---
+++ src/tests/mocks/CarMock.ts
@@ -0,0 +1,29 @@
+import { CarDocument } from "../../interfaces/CarInterface"
+
+export const carMockRequest = {
+  model: 'Ferrari Maranello',
+  year: 1963,
+  color: 'red',
+  buyValue: 3500000,
+  seatsQty: 2,
+  doorsQty: 2
+} as CarDocument;
+
+export const carMockResponse = {
+  model: 'Ferrari Maranello',
+  year: 1963,
+  color: 'red',
+  buyValue: 3500000,
+  seatsQty: 2,
+  doorsQty: 2,
+  _id: "62c7392922b7499c89e6832c"
+} as CarDocument;
+
+export const carMockRequestError = {
+  model: '',
+  year: 0,
+  color: '',
+  buyValue: 0,
+  seatsQty: 0,
+  doorsQty: 0
+} as CarDocument;
