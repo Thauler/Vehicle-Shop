f1ba4a855ac0bcb188d1720d61fff7be28562c53
Thauler <thauler98@gmail.com>
2022-07-09 12:31:54 -0300

    feat(carcontroller.test.ts): controller tests
    In case of success: test if the method create at controller when is send a request returns a
    status-code 201 and a car object
     In case of error: if is passed a request with an empty object at
    method create, then is expected to return a status 400
     Internal server error: tests if at
    controller none of conditions is used, then pops a catch error with status 500

--- src/tests/unit/controllers/CarController.test.ts
+++ src/tests/unit/controllers/CarController.test.ts
@@ -1,32 +1,106 @@
-import * as sinon from 'sinon';
+import sinon, { SinonStub } from 'sinon';
 import chai from 'chai';
+const { expect } = chai;
 import chaiHttp = require('chai-http');
+import { Response } from 'express';
+
 import CarController from '../../../controllers/CarController';
 import { Car } from '../../../interfaces/CarInterface';
-import { carMockRequest, carMockResponse } from '../mocks/CarMock';
-import { Request, Response } from 'express';
-
+import { carMockRequest, carMockResponse } from '../../mocks/CarMock';
+import { CarModelMock, CarServiceMock } from '../../mocks/CarClassMocks';
+import carMongooseModel from '../../../models/Schemas/CarSchema';
+import { RequestWithBody } from '../../../controllers';
 
 chai.use(chaiHttp);
 
-const { expect } = chai;
+const carModelMock = new CarModelMock(carMongooseModel);
+const carService = new CarServiceMock(carModelMock);
+const carController = new CarController(carService);
 
 describe('1 - Testing car controller', () => {
-  const carController = new CarController();
-  const request = {} as Request<Car>;
-  const response = {} as Response;
+  
   describe('A - Testing method create', () => {
-   before(async() => {
-      request.body = carMockRequest;
-      response.status = sinon.stub().returns(response);
-      response.json = sinon.stub().returns(response);
-      sinon.stub(carController.service, 'create').resolves(carMockResponse);
-    });
-    after(() => sinon.restore());
-    it('In case of success', async () => {
-      const createdCar = await carController.service.create(carMockRequest);
 
-      expect(createdCar).deep.equal(carMockResponse);
+    const requestWithBody = {} as RequestWithBody<Car>;
+    const response = {} as Response;
+
+    describe('Testing /cars route', () => {
+
+      it('- Route /cars', async () => {
+        expect(carController.route).to.be.equals('/cars');
+      });
     });
+
+    describe('A.1 - In case of success', () => { 
+
+      before(async () => {
+        requestWithBody.body = carMockRequest;
+        response.status = sinon.stub().returns(response);
+        response.json = sinon.stub().returns(response);
+        sinon.stub(carService, 'create').resolves(carMockRequest);
+      });
+
+      after(() => (carService.create as SinonStub).restore());
+
+      it('- Controller method create return status 201 and the Car object', async () => {
+        await carController.create(requestWithBody, response);
+        
+        expect((response.status as SinonStub).calledWith(201));
+        expect((response.json as SinonStub).calledWith(carMockResponse));
+      });
+    });
+
+    describe('A.2 - In case of failure', () => {
+
+      before(async () => {
+
+        response.status = sinon.stub().returns(response);
+        response.json = sinon.stub().returns({ error: 'error' } as any);
+        sinon.stub(carService, 'create').resolves(undefined);
+      });
+
+      after(() => (carService.create as SinonStub).restore());
+
+        it('- Controller method create return status 400 and an error message when is requested without a body', async() => {
+          await carController.create(requestWithBody, response);
+
+        expect((response.status as SinonStub).calledWith(400)).to.be.true;
+        });
+      })
+
+          describe('A.2 - In case of failure', () => {
+
+      before(async () => {
+
+        response.status = sinon.stub().returns(response);
+        response.json = sinon.stub().returns({ error: 'error' } as any);
+        sinon.stub(carService, 'create').resolves({ error: 'error' } as any);
+      });
+
+      after(() => (carService.create as SinonStub).restore());
+
+        it('- Controller method create return status 400 and an error message when is requested without a body', async() => {
+          await carController.create(requestWithBody, response);
+
+        expect((response.status as SinonStub).calledWith(400)).to.be.true;
+        });
+      })
+
+      describe('Internal server Error', () => {
+
+            before(async () => {
+              response.status = sinon.stub().returns(response);
+              response.json = sinon.stub().returns(response);
+              sinon.stub(carService, 'create').throws();
+            });
+
+            after(() => (carService.create as SinonStub).restore());
+
+        it('- Controller method create return status 500', async() => {
+          await carController.create(requestWithBody, response);
+          
+          expect((response.status as SinonStub).calledWith(500));
+        })
+      })
     })
   })
