b6f6b81e277ae35af47443180a57dd5a2f3e3ebe
Thauler <thauler98@gmail.com>
2022-07-09 12:16:52 -0300

    feat(carservice.test.ts): testing error case
    tests if at the request is passed a object with wrong datas and expects zod error is not empty

--- src/tests/unit/services/CarService.test.ts
+++ src/tests/unit/services/CarService.test.ts
@@ -1,24 +1,37 @@
 import { expect } from 'chai';
+import { SinonStub } from 'sinon';
 import sinon from 'sinon';
+import { carMongooseModel } from '../../../models/Schemas/CarSchema';
 import CarService from '../../../services/CarService';
-import { carMockRequest, carMockResponse } from '../mocks/CarMock';
+import { CarModelMock } from '../../mocks/CarClassMocks';
+import { carMockRequest, carMockRequestError, carMockResponse } from '../../mocks/CarMock';
+import { ServiceError } from '../../../services';
+
+  const carModelMock = new CarModelMock(carMongooseModel);
+  const carService = new CarService(carModelMock);
 
 describe('1 - Testing car service', () => {
-  const carService = new CarService();
-
+  
   describe('A - Testing mehtod creates', () => {
+    
     before(async () => {
-      sinon.stub(carService.model, 'create').resolves(carMockResponse);
+      sinon.stub(carModelMock, 'create').resolves(carMockResponse);
     });
-    after(() => sinon.restore());
+    
+    after(() => (carModelMock.create as SinonStub).restore());
+    
     it('In case of success', async () => {
       const createdCar = await carService.create(carMockRequest);
-
+      
       expect(createdCar).to.be.an('object');
       expect(createdCar).deep.equal(carMockResponse);
     });
-    // it('In case of failure', async () => {
-
-    // })
+  });
+  describe('In case of error', () => {
+  
+    it('Return Error Zod create' , async () => {
+      const createdCar = await carService.create(carMockRequestError) as ServiceError;
+      expect(createdCar.error.isEmpty).to.be.false;
+    });
   });
 })
