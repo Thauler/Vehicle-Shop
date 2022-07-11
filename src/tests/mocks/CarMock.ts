b6d278aa238a2d6827b86fca65b8e617431ed347
Thauler <thauler98@gmail.com>
2022-07-11 12:00:27 -0300

    feat(carmock.ts): adds mock response
    adds a mock for method read with an array of car response

--- src/tests/mocks/CarMock.ts
+++ src/tests/mocks/CarMock.ts
@@ -27,3 +27,13 @@ export const carMockRequestError = {
   seatsQty: 0,
   doorsQty: 0
 } as CarDocument;
+
+export const allCarMockResponse = [{
+  model: 'Ferrari Maranello',
+  year: 1963,
+  color: 'red',
+  buyValue: 3500000,
+  seatsQty: 2,
+  doorsQty: 2,
+  _id: "62c7392922b7499c89e6832c"
+}] as (CarDocument & { _id: string })[];
