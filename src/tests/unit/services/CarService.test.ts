f46a4ba3cdc5c0a2ea2a4e3fbed10d9149d43915
Thauler <thauler98@gmail.com>
2022-07-11 12:14:13 -0300

    test(carservice.test.ts): test method read at service layer
    adds a test for method read at service layer in case of success and expects the response to be an
    array and to be equals a mock array of object with a type like cardocument

--- src/tests/unit/services/CarService.test.ts
+++ src/tests/unit/services/CarService.test.ts
@@ -4,34 +4,52 @@ import sinon from 'sinon';
 import { carMongooseModel } from '../../../models/Schemas/CarSchema';
 import CarService from '../../../services/CarService';
 import { CarModelMock } from '../../mocks/CarClassMocks';
-import { carMockRequest, carMockRequestError, carMockResponse } from '../../mocks/CarMock';
+import { allCarMockResponse, carMockRequest, carMockRequestError, carMockResponse } from '../../mocks/CarMock';
 import { ServiceError } from '../../../services';
 
   const carModelMock = new CarModelMock(carMongooseModel);
   const carService = new CarService(carModelMock);
 
-describe('1 - Testing car service', () => {
-  
-  describe('A - Testing mehtod creates', () => {
-    
-    before(async () => {
-      sinon.stub(carModelMock, 'create').resolves(carMockResponse);
-    });
-    
-    after(() => (carModelMock.create as SinonStub).restore());
-    
-    it('In case of success', async () => {
-      const createdCar = await carService.create(carMockRequest);
+describe('Service: - Testing car service', () => {
+
+  describe('A) - Testing mehtod creates', () => {
+    describe('- In case of success', () => {
+
+      before(async () => {
+        sinon.stub(carModelMock, 'create').resolves(carMockResponse);
+      });
       
-      expect(createdCar).to.be.an('object');
-      expect(createdCar).deep.equal(carMockResponse);
+      after(() => (carModelMock.create as SinonStub).restore());
+      
+      it('Return the car object', async () => {
+        const createdCar = await carService.create(carMockRequest);
+        
+        expect(createdCar).to.be.an('object');
+        expect(createdCar).deep.equal(carMockResponse);
+      });
+    })
+    describe('- In case of error', () => {
+      
+      it('Return Error Zod create' , async () => {
+        const createdCar = await carService.create(carMockRequestError) as ServiceError;
+        expect(createdCar.error.isEmpty).to.be.false;
+      });
     });
   });
-  describe('In case of error', () => {
-  
-    it('Return Error Zod create' , async () => {
-      const createdCar = await carService.create(carMockRequestError) as ServiceError;
-      expect(createdCar.error.isEmpty).to.be.false;
+    
+    describe('B) - Testing mehtod read', () => {
+    
+    before(async () => {
+      sinon.stub(carModelMock, 'read').resolves(allCarMockResponse);
+    });
+    
+    after(() => (carModelMock.read as SinonStub).restore());
+    
+    it('In case of success', async () => {
+      const allCars = await carService.read();
+      
+      expect(allCars).to.be.an('array');
+      expect(allCars).deep.equal(allCarMockResponse);
     });
   });
 })
