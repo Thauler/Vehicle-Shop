54ffa3c8377c3a61e0ed7038ac7e9abe159700a7
Thauler <thauler98@gmail.com>
2022-07-26 23:29:47 -0300

    feat(src/routes): implements delete on route /cars/:id

--- src/routes/Router.ts
+++ src/routes/Router.ts
@@ -11,5 +11,6 @@ export default class Routers<T> {
   public addRoute(controller: Controller<T>, route: string = controller.route) {
     this.router.post(route, controller.create);
     this.router.get(route, controller.read);
+    this.router.delete(`${route}/:id`, controller.delete);
   }
 }
