2acf5fd35013fc5e3c3912ea2a3f5ad698c36f98
Thauler <thauler98@gmail.com>
2022-07-26 23:04:56 -0300

    feat(src/tests/mocks): implements method delete on mock classes
    implements the method delete on mock model and service

--- src/tests/mocks/CarClassMocks.ts
+++ src/tests/mocks/CarClassMocks.ts
@@ -5,15 +5,21 @@ import { allCarMockRequest, carMockRequest } from "./CarMock";
 
 
 export class CarModelMock extends GenericModel<CarDocument> {
+
   read = async (): Promise<CarDocument[]> => allCarMockRequest;
 
   create = async (_object: CarDocument): Promise<CarDocument> => carMockRequest;
+
+  delete = async (_id: string): Promise<CarDocument | null> => carMockRequest.id;
 }
 
 export class CarServiceMock extends Service<CarDocument> {
+
   read = async (): Promise<CarDocument[]> => allCarMockRequest;
 
   create = async (_object: CarDocument): Promise<CarDocument> => {
     return carMockRequest;
   }
+
+  delete = async (_id: string): Promise<CarDocument | null> => carMockRequest.id;
 }
