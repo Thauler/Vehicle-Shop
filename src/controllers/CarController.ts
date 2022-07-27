12904634508ad2d737d4813dbff76bf9a47e4e95
Thauler <thauler98@gmail.com>
2022-07-26 23:45:28 -0300

    feat(src/controllers): implements the method delete from services passing an id from request params
    If the id length is different from 24 characters return a response status 400 an a message error:
    "Id must have 24 hexadecimal characters"
    
     if the request is undefined then the return of the
    response with status 404 with a message error:   "Object not found"
    
     If is successesful returns
    a status code 204 and an empty response
    
     And if is a internal error the response status 500 an
    an error message: "Internal Server Error"

--- src/controllers/CarController.ts
+++ src/controllers/CarController.ts
@@ -66,4 +66,24 @@ export default class CarController extends Controller<Car> {
   //     return res.status(500).json({ error: this.errors.internal });
   //   }
   // };
+
+  delete = async (
+    req: Request<{ id: string }>,
+    res: Response<Car | ResponseError>,
+  ): Promise<typeof res> => {
+    try {
+      const { id } = req.params;
+      if (id.length !== 24) {
+        return res.status(400)
+          .json({ error: this.errors.incorrectId });
+      }
+      const result = await this.$service.delete(id);
+      if (!result) {
+        return res.status(404).json({ error: this.errors.notFound });
+      }
+      return res.status(204).json();
+    } catch (e) {
+      return res.status(500).json({ error: this.errors.internal });
+    }
+  };
 }
