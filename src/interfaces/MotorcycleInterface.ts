ea776caa1e0616b850a3f46cde49ab88a137dec3
Thauler <thauler98@gmail.com>
2022-08-07 20:21:16 -0300

    feat(src/interfaces): implements the Motorcycle interface

---
+++ src/interfaces/MotorcycleInterface.ts
@@ -0,0 +1,12 @@
+import { Document } from 'mongoose';
+import { z } from 'zod';
+import { VehicleSchema } from './VehicleInterface';
+
+export const MotorcycleSchema = VehicleSchema.extend({
+  category: z.enum(['Street', 'Custom', 'Trail']),
+  engineCapacity: z.number().max(2500),
+});
+
+export type Motorcycle = z.infer<typeof MotorcycleSchema>;
+
+export interface MotorcycleDocument extends Motorcycle, Document { }
