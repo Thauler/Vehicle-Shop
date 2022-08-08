c182cbc1efcfd5c5e12f477610a72e4836189fae
Thauler <thauler98@gmail.com>
2022-08-07 22:01:05 -0300

    feat(src/server.ts): adds the motorcycle routers

--- src/server.ts
+++ src/server.ts
@@ -2,14 +2,19 @@ import Routers from './routes/Router';
 import App from './app';
 
 import { Car } from './interfaces/CarInterface';
-import carFactoryController from './factories/CarFactory';
+import carFactoryController from './factories/Car/CarFactory';
+import motorcycleController from './factories/Motorcycle/MotorcycleFactory';
+import { Motorcycle } from './interfaces/MotorcycleInterface';
 
 const server = new App();
 
 const carRouter = new Routers<Car>();
+const motorcycleRouter = new Routers<Motorcycle>();
 
 carRouter.addRoute(carFactoryController);
+motorcycleRouter.addRoute(motorcycleController);
 
 server.addRouter(carRouter.router);
+server.addRouter(motorcycleRouter.router);
 
 export default server;
